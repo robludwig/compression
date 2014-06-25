95ab22b6d32f0742d7915171794f365a89d1c818
Robert Ludwig <rob.ludwig@lyricallabs.com>
2014-06-25 14:20:25 -0500

    strip out any punctionation, will need to add it back in

--- compress.py
+++ compress.py
@@ -8,11 +8,14 @@ def compress(text):
 		for word in line.split(" "):
 			#print "compressing \"%s\" " % word
 			lowerword = word.lower().strip()
-			if lowerword in dictionary:
-				compressed_data += str(dictionary.index(lowerword))
+			#strip out the punctuation so we
+			raw_word = "".join([letter for letter in lowerword if letter.isalpha()])
+			print "lemmatized the word to %s " % raw_word
+			if raw_word in dictionary:
+				compressed_data += str(dictionary.index(raw_word))
 			else:
-				dictionary.append(lowerword)
-				compressed_data += str(dictionary.index(lowerword))
+				dictionary.append(raw_word)
+				compressed_data += str(dictionary.index(raw_word))
 			if word == word.upper():
 				compressed_data += "!" #capitalize the whole word
 			elif word.istitle():
