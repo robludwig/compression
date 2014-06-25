8a25cf84392a09c5e059b2a99ed54f827433fdb1
Robert Ludwig <rob.ludwig@lyricallabs.com>
2014-06-25 14:13:09 -0500

    working compression, but cheats on punctuation

--- compress.py
+++ compress.py
@@ -4,14 +4,30 @@ def compress(text):
 	dictionary = []
 	data = []
 	compressed_data = ""
+	for line in text:
+		for word in line.split(" "):
+			#print "compressing \"%s\" " % word
+			lowerword = word.lower().strip()
+			if lowerword in dictionary:
+				compressed_data += str(dictionary.index(lowerword))
+			else:
+				dictionary.append(lowerword)
+				compressed_data += str(dictionary.index(lowerword))
+			if word == word.upper():
+				compressed_data += "!" #capitalize the whole word
+			elif word.istitle():
+				compressed_data += "^" #capitalize just the first letter
+			compressed_data += " "
+		compressed_data += 'R ' #newline after each line
 	
+	compressed_data += 'E ' #EOR
 	return dictionary, compressed_data
 	
 	
 if __name__ == "__main__":
-	inputtext = open(sys.argv[1]).read()
+	inputtext = open(sys.argv[1]).readlines()
 	dictionary, compressed_data = compress(inputtext)
-	print len(dictionary),
+	print len(dictionary)
 	for word in dictionary:
 		print word
 	print compressed_data
