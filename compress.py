f7c511c5f5000dd09e9410e94ec9f50cdcdf7be1
Robert Ludwig <rob.ludwig@lyricallabs.com>
2014-06-25 14:26:05 -0500

    handle punctuation properly...

--- compress.py
+++ compress.py
@@ -1,5 +1,7 @@
 import sys
 
+VALID_PUNCTUATION = [ '.', ',', '?', '!', ';', ':']
+
 def compress(text):
 	dictionary = []
 	data = []
@@ -8,22 +10,35 @@ def compress(text):
 		for word in line.split(" "):
 			#print "compressing \"%s\" " % word
 			lowerword = word.lower().strip()
-			#strip out the punctuation so we
+			
+			#strip out the punctuation so we don't cheat on the compression
 			raw_word = "".join([letter for letter in lowerword if letter.isalpha()])
-			print "lemmatized the word to %s " % raw_word
+			#print "lemmatized the word to %s " % raw_word
+			
+			#put it in the dictionary and get the index
 			if raw_word in dictionary:
 				compressed_data += str(dictionary.index(raw_word))
 			else:
 				dictionary.append(raw_word)
 				compressed_data += str(dictionary.index(raw_word))
+				
+			#handle the capitalization...
 			if word == word.upper():
 				compressed_data += "!" #capitalize the whole word
 			elif word.istitle():
 				compressed_data += "^" #capitalize just the first letter
-			compressed_data += " "
+			
+			#handle the punctuation... rules are you get one symbol per word
+			for symbol in VALID_PUNCTUATION:
+				if symbol in word:
+					compressed_data += " " + symbol
+					break #TODO: add error handling if you have more than one symbol...
+			
+			#add space and move on to the next thing...
+			compressed_data += " "		
 		compressed_data += 'R ' #newline after each line
 	
-	compressed_data += 'E ' #EOR
+	compressed_data += 'E ' #EOF
 	return dictionary, compressed_data
 	
 	
